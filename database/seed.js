1283b100a94e1fbf9be5d0a4583e039b1e488adb
Kyle Davies <kdavies311@gmail.com>
2019-03-14 16:43:09 -0700

    updated seeding to write tocsv

--- database/seed.js
+++ database/seed.js
@@ -1,9 +1,9 @@
-var config = require("../knexfile");
-var env = "development";
-var knex = require("knex")(config[env]);
+// var config = require("../knexfile");
+// var env = "development";
+// var knex = require("knex")(config[env]);
 const faker = require("faker");
 const fs = require('fs');
-// const pathToFile = __dirname + '/testSeed.csv';
+const pathToFile = __dirname + '/testSeed.csv';
 // const cassandra = require('cassandra-driver');
 // const cassanKnex = require("cassanknex");
 
@@ -11,35 +11,35 @@ const fs = require('fs');
 // SEEDING FUNCTION FOR CASSANDRA ******************
 
 
-// function writeFakeData() {
-//   //empty the file first
-//   fs.writeFileSync(pathToFile, 'reviewid|createdat|h_yes|h_no|nickname|rating|review\n');
+function writeFakeData() {
+
+  fs.writeFileSync(pathToFile, 'reviewid|createdat|h_yes|h_no|nickname|rating|review\n');
 
 
-//   var chunk = 0;
-//   var agregateQuery = '';
-//   for (var j = 0; j < 5; j++) {
-//     chunk++;
-//      agregateQuery +=
-//          Math.floor(Math.random() * (2000000))+ '|' +
-//         '"' + faker.date.past() + '"|' +
-//          + Math.floor(Math.random()* (40)) + '|' +
-//          + Math.floor(Math.random()* (40)) + '|' +
-//         '"' + faker.internet.userName() + '"|' +
-//         Math.floor(Math.random() * (5)) + '|' +
-//         '"' + faker.lorem.sentences() + '"\n'
+  var chunk = 0;
+  var agregateQuery = '';
+  for (var j = 0; j < 1000000; j++) {
+    chunk++;
+     agregateQuery +=
+         Math.floor(Math.random() * (10000000))+ '|' +
+        '"' + faker.date.past() + '"|' +
+         + Math.floor(Math.random()* (40)) + '|' +
+         + Math.floor(Math.random()* (40)) + '|' +
+        '"' + faker.internet.userName() + '"|' +
+        Math.floor(Math.random() * (5)) + '|' +
+        '"' + faker.lorem.sentences() + '"\n'
 
-//        //  if (chunk >= 1000) {
-//          fs.appendFileSync(pathToFile, agregateQuery)
-//        //   chunk = 0;
-//        //   agregateQuery = '';
-//        // }
+         if (chunk >= 1000) {
+         fs.appendFileSync(pathToFile, agregateQuery)
+          chunk = 0;
+          agregateQuery = '';
+        }
 
-//   }
-//   console.log('seeded')
-// }
+  }
+  console.log('seeded')
+}
 
-// writeFakeData();
+writeFakeData();
 
 
 
@@ -72,36 +72,36 @@ const fs = require('fs');
 
 //}
 
-const createFakeReview = () => {
+// const createFakeReview = () => {
 
 
-  return {
+//   return {
 
-    nickname: faker.internet.userName(),
-    review: faker.lorem.sentences(),
-    rating: Math.floor(Math.random() * (5)),
-    createdat: faker.date.past(),
-    index: Math.floor(Math.random() * (2000)),
-    h_yes: Math.floor(Math.random()* (40)),
-    h_no: Math.floor(Math.random()* (40)),
-  }
+//     nickname: faker.internet.userName(),
+//     review: faker.lorem.sentences(),
+//     rating: Math.floor(Math.random() * (5)),
+//     createdat: faker.date.past(),
+//     index: Math.floor(Math.random() * (2000)),
+//     h_yes: Math.floor(Math.random()* (40)),
+//     h_no: Math.floor(Math.random()* (40)),
+//   }
 
-}
+// }
 
-const seedReviews= () => {
-  const fakeReviews = [];
-  const total = 8000;
-  for (var j = 0; j < total; j++) {
-    fakeReviews.push(createFakeReview());
-  }
-  return knex("reviews").insert(fakeReviews)
-    .then(() => {
-      console.log('seeding completed')
-    })
-    .catch((err)=> {console.log('error: ', err)})
+// const seedReviews= () => {
+//   const fakeReviews = [];
+//   const total = 8000;
+//   for (var j = 0; j < total; j++) {
+//     fakeReviews.push(createFakeReview());
+//   }
+//   return knex("reviews").insert(fakeReviews)
+//     .then(() => {
+//       console.log('seeding completed')
+//     })
+//     .catch((err)=> {console.log('error: ', err)})
 
 
-}
+// }
 
 
  /* Still needs to be optizimed in order to seed multiple batches one after another. Othewise we need to invoke each function far too many times in order to get 10M && 40M
@@ -109,6 +109,6 @@ const seedReviews= () => {
 
 
 //will need to run many times.
-seedReviews();
+//seedReviews();
 
 
